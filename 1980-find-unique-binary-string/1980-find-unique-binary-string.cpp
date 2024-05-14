3b8753a36d0d22b96a7ef4dc3749e868d87313e4
Aman Kumar <trulyaman25@gmail.com>
2024-05-15 01:00:59 +0530

    Time: 8 ms (21.22%), Space: 13.2 MB (28.03%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string formBinary(int number, int length){
+        if(number == 0){
+            return "0";
+        }
+
+        string binary = "";
+        while(number){
+            int remainder = number % 2;
+            binary = to_string(remainder) + binary;
+            number /= 2;
+        }
+
+        while(binary.length() < length){
+            binary = "0" + binary;
+        }
+
+        return binary;
+    }
+
+    string findDifferentBinaryString(vector<string>& nums) {
+        sort(nums.begin(), nums.end());
+        unordered_map<string, int> hashMap;
+        for(int i = 0; i < nums.size(); i++){
+            hashMap[nums[i]]++;
+        }
+
+        int maxInt = pow(2, nums.size()) - 1;
+
+        while(maxInt >= 0){
+            string target = formBinary(maxInt--, nums.size());
+            if(hashMap[target] <= 0){
+                return target;
+            }
+        }
+
+        return "";
+    }
+};
