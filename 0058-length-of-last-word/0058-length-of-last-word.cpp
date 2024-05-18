40f13abdba46eebd5e28cb3f3a05af6b806fb182
Aman Kumar <trulyaman25@gmail.com>
2024-05-18 19:34:03 +0530

    Time: 2 ms (53.3%), Space: 7.6 MB (86.95%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int count = 0;
-        bool flag = false;
+        int lengthCount = 0;
         for(int i = s.length() - 1; i >= 0; i--){
-            if(s[i] == ' '){
-                if(!flag){
-                    continue;
-                } else {
-                    break;
-                }
-            } else {
-                count++;
-                flag = true;
+            if(s[i] != ' '){
+                lengthCount++;
+            }
+            if(lengthCount > 0 && s[i] == ' '){
+                return lengthCount;
             }
         }
-
-        return count;
+        return lengthCount;
     }
 };
