6bbd6364ed6f536789bab631ad1c1ec2d3825b75
Aman Kumar <trulyaman25@gmail.com>
2024-05-18 15:35:34 +0530

    Time: 11 ms (37.67%), Space: 9.9 MB (6.04%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        int length = t.length() - 1;
+        unordered_map<char, int> hashMap;
+        for(int i = 0;  i < length + 1; i++){
+            hashMap[s[i]]++;
+            hashMap[t[i]]--;
+        }
+
+        for(auto it : hashMap){
+            if(it.second != 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
