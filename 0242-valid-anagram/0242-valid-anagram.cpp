ebdd59521f4c83be99d6f098dfff3c7b9bec4d19
Aman Kumar <trulyaman25@gmail.com>
2024-05-18 15:41:47 +0530

    Time: 13 ms (26.08%), Space: 8.4 MB (96.61%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,35 +1,35 @@
-//Approach 1 (Genuine)
-class Solution {
-public:
-    bool isAnagram(string s, string t) {
-        if(s.length() != t.length()){
-            return false;
-        }
-
-        int length = t.length() - 1;
-        unordered_map<char, int> hashMap;
-        for(int i = 0;  i < length + 1; i++){
-            hashMap[s[i]]++;
-            hashMap[t[i]]--;
-        }
-
-        for(auto it : hashMap){
-            if(it.second != 0){
-                return false;
-            }
-        }
-
-        return true;
-    }
-};
-
-
-//Approach 2
+// Approach 1 (Genuine)
 // class Solution {
 // public:
 //     bool isAnagram(string s, string t) {
-//         sort(s.begin(), s.end());
-//         sort(t.begin(), t.end());
-//         return s == t;
+//         if(s.length() != t.length()){
+//             return false;
+//         }
+
+//         int length = t.length() - 1;
+//         unordered_map<char, int> hashMap;
+//         for(int i = 0;  i < length + 1; i++){
+//             hashMap[s[i]]++;
+//             hashMap[t[i]]--;
+//         }
+
+//         for(auto it : hashMap){
+//             if(it.second != 0){
+//                 return false;
+//             }
+//         }
+
+//         return true;
 //     }
 // };
+
+
+//Approach 2
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
+    }
+};
