e20077adf5c8b1adb592914184484044e9204869
Aman Kumar <trulyaman25@gmail.com>
2024-05-18 18:22:36 +0530

    Time: 82 ms (39%), Space: 71 MB (63.86%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,35 +1,35 @@
 //BRUTE FORCE SOLUTION
-class Solution {
-public:
-    vector<int> replaceElements(vector<int>& arr) {
-        for(int i = 0; i < arr.size(); i++){
-            int maxElement = INT_MIN;
-            for(int j = i + 1; j < arr.size(); j++){
-                if(arr[j] > maxElement){
-                    maxElement = arr[j];
-                }
-            }
-            arr[i] = maxElement;
-        }
-        arr.back() = -1;
-
-        return arr;
-    }
-};
-
-//OPTIMIZED SOLUTION
 // class Solution {
 // public:
 //     vector<int> replaceElements(vector<int>& arr) {
-//         int maxElement = -1;
-//         for(int i = arr.size() - 1; i >= 0; i--){
-//             int refferenceValue = arr[i];
-//             arr[i] = maxElement;
-//             if(refferenceValue > maxElement){
-//                 maxElement = refferenceValue;
+//         for(int i = 0; i < arr.size(); i++){
+//             int maxElement = INT_MIN;
+//             for(int j = i + 1; j < arr.size(); j++){
+//                 if(arr[j] > maxElement){
+//                     maxElement = arr[j];
+//                 }
 //             }
+//             arr[i] = maxElement;
 //         }
+//         arr.back() = -1;
 
 //         return arr;
 //     }
 // };
+
+//OPTIMIZED SOLUTION
+class Solution {
+public:
+    vector<int> replaceElements(vector<int>& arr) {
+        int maxElement = -1;
+        for(int i = arr.size() - 1; i >= 0; i--){
+            int refferenceValue = arr[i];
+            arr[i] = maxElement;
+            if(refferenceValue > maxElement){
+                maxElement = refferenceValue;
+            }
+        }
+
+        return arr;
+    }
+};
