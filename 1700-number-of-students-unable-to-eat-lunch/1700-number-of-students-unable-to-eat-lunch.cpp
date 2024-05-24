be8dc76416e89245aef3b84264459550d81d34d2
Aman Kumar <trulyaman25@gmail.com>
2024-05-24 11:13:26 +0530

    Time: 3 ms (36.28%), Space: 10.9 MB (13.72%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -12,7 +12,7 @@ public:
         }
 
         int count = 0;
-        while(!stack.empty() && count < students.size()){
+        while(!stack.empty() && count < queue.size()){
             if(queue.front() == stack.top()){
                 queue.pop();
                 stack.pop();
