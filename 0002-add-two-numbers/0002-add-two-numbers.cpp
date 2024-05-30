03035632517a2524115ef44f4b79325069722c6b
Aman Kumar <trulyaman25@gmail.com>
2024-05-30 14:07:18 +0530

    Time: 8 ms (98.2%), Space: 76 MB (83.62%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,13 +1,6 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+//BRUTE FORCE SOLUTION
+
+/*
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* headOne, ListNode* headTwo) {
@@ -53,6 +46,62 @@ public:
                 ListNode* newNode = new ListNode(sum);
                 mover->next = newNode;
                 mover = mover->next;
+                tempHeadTwo = tempHeadTwo->next;
+            }
+        }
+
+        if(carryBit){
+            ListNode* newNode = new ListNode(carryBit);
+            mover->next = newNode;
+            mover = mover->next;
+        }
+
+        return newHead;
+    }
+};
+*/
+
+
+//OPTIMIZED SOLUTION
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* headOne, ListNode* headTwo) {
+        ListNode* tempHeadOne = headOne;
+        ListNode* tempHeadTwo = headTwo;
+
+        bool carryBit = false;
+        int sum = tempHeadOne->val + tempHeadTwo->val;
+        carryBit = sum >= 10;
+        sum = sum % 10;
+
+        tempHeadOne = tempHeadOne->next;
+        tempHeadTwo = tempHeadTwo->next;
+
+        ListNode* newHead = new ListNode(sum);
+        ListNode* mover = newHead;
+        while(tempHeadOne || tempHeadTwo){
+
+            int sum = carryBit;
+
+            if(tempHeadOne){
+                sum += tempHeadOne->val;
+            }
+
+            if(tempHeadTwo){
+                sum += tempHeadTwo->val;
+            }
+            
+            carryBit = sum >= 10;
+            sum = sum % 10;
+            ListNode* newNode = new ListNode(sum);
+
+            mover->next = newNode;
+            mover = mover->next;
+
+            if(tempHeadOne != NULL){
+                tempHeadOne = tempHeadOne->next;
+            }
+            if(tempHeadTwo != NULL){
                 tempHeadTwo = tempHeadTwo->next;
             }
         }
