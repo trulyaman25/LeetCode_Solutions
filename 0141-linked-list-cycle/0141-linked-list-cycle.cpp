2c52490cdb2e024f867d82678d2585a413dd83ef
Aman Kumar <trulyaman25@gmail.com>
2024-05-30 14:43:13 +0530

    Time: 7 ms (84.53%), Space: 10.6 MB (63.98%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -1,24 +1,37 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+//BRUTE FORCE
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//         unordered_map<ListNode*, int> hashMap;
+//         ListNode* tempHead = head;
+//         while(tempHead){
+//             if(hashMap.count(tempHead) == 0){
+//                 hashMap[tempHead]++;
+//             } else {
+//                 return true;
+//             }
+//             tempHead = tempHead->next;
+//         }
+//         return false;
+//     }
+// };
+
+//OPTIMIZED SOLUTION USING 2 POINTERS
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        unordered_map<ListNode*, int> hashMap;
-        ListNode* tempHead = head;
-        while(tempHead){
-            if(hashMap.count(tempHead) == 0){
-                hashMap[tempHead]++;
-            } else {
+        ListNode* slowPointer = head;
+        ListNode* fastPointer = head;
+
+        while(fastPointer && fastPointer->next){
+            slowPointer = slowPointer->next;
+            fastPointer = fastPointer->next->next;
+            
+            if(slowPointer == fastPointer){
                 return true;
             }
-            tempHead = tempHead->next;
         }
+
         return false;
     }
 };
