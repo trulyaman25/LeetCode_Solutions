3d309bd13ad4f8ee21105c7b135aecfa7779ee81
Aman Kumar <trulyaman25@gmail.com>
2024-05-31 15:42:28 +0530

    Time: 34 ms (60.77%), Space: 34 MB (67.63%) - LeetHub

--- 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.cpp
+++ 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.cpp
@@ -1,14 +1,5 @@
 class Solution {
 public:
-    int gcd(int numberOne, int numberTwo) {
-        while (numberTwo) {
-            int temp = numberTwo;
-            numberTwo = numberOne % numberTwo;
-            numberOne = temp;
-        }
-        return numberOne;
-    }
-
     ListNode* insertGreatestCommonDivisors(ListNode* head) {
         if(head == NULL || head->next == NULL){
             return head;
@@ -16,7 +7,7 @@ public:
 
         ListNode* tempHead = head;
         while(tempHead->next){
-            int requiredGCD = gcd(tempHead->val, tempHead->next->val);
+            int requiredGCD = __gcd(tempHead->val, tempHead->next->val);
             ListNode* newNode = new ListNode(requiredGCD, tempHead->next);
 
             tempHead->next = newNode;
