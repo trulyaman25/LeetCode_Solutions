fcb897ca81895aa82286cdca1c0f1bc4d31df2d7
Aman Kumar <trulyaman25@gmail.com>
2024-06-02 16:34:31 +0530

    Time: 14 ms (82.25%), Space: 27.1 MB (21.89%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -4,11 +4,7 @@ public:
         int pointerOne = 0;
         int pointerTwo = s.size() - 1;
         while(pointerOne < pointerTwo){
-            char tempStorage = s[pointerOne];
-            s[pointerOne] = s[pointerTwo];
-            s[pointerTwo] = tempStorage;
-            pointerOne++;
-            pointerTwo--;
+            swap(s[pointerOne++], s[pointerTwo--]);
         }
     }
 };
