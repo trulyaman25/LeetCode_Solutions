f9dec75bb84b34c528d8eaacedd0993474f8de8e
Aman Kumar <trulyaman25@gmail.com>
2024-06-02 22:30:34 +0530

    Time: 321 ms (16.67%), Space: 125.7 MB (16.67%) - LeetHub

--- 3169-count-days-without-meetings/3169-count-days-without-meetings.cpp
+++ 3169-count-days-without-meetings/3169-count-days-without-meetings.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
     int countDays(int days, vector<vector<int>>& meetings) {
+        if (meetings.empty()) return days;
+
         sort(meetings.begin(), meetings.end());
 
         vector<vector<int>> resultantVector;
-        resultantVector.push_back(meetings[0]);
+     resultantVector.push_back(meetings[0]);
 
-        for(int i = 1; i < meetings.size(); i++){
-            if(resultantVector.back()[1] == meetings[i][0]){
-                resultantVector.back()[1] = max(resultantVector.back()[1], meetings[i][1]);
+        for (int i = 1; i < meetings.size(); ++i) {
+            if (meetings[i][0] <= resultantVector.back()[1]) {
+             resultantVector.back()[1] = max(resultantVector.back()[1], meetings[i][1]);
             } else {
-                resultantVector.push_back(meetings[i]);
+             resultantVector.push_back(meetings[i]);
             }
         }
 
-        int count = 0;
-        for(int i = 0; i < resultantVector.size() - 1; i++){
-            if(resultantVector[i][1] < resultantVector[i + 1][0]){
+        int count = resultantVector[0][0] - 1;
+        for (int i = 0; i < resultantVector.size() - 1; ++i) {
+            if (resultantVector[i][1] < resultantVector[i + 1][0] - 1) {
                 count += resultantVector[i + 1][0] - resultantVector[i][1] - 1;
             }
         }
-
-        if(resultantVector[0][0] != 0){
-            count += resultantVector[0][0] - 1;
-        }
-
-        if(resultantVector.back()[1] != days){
-            count += days - resultantVector.back()[1];
-        }
+        count += days - resultantVector.back()[1];
 
         return count;
     }
