28bd35a6a10da94275593f873ba347290df53bae
Aman Kumar <trulyaman25@gmail.com>
2024-06-11 17:04:27 +0530

    Time: 102 ms (95.5%), Space: 80.9 MB (60.36%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,40 +1,13 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        if(k == 0){
-            return false;
-        }
-        if(nums.size() == 2 && nums[0] == nums[1]){
-            return true;
-        }
-        if(k + 1 > nums.size()){
-            return false;
-        }
-
-        int pointerOne = 0;
-        int pointerTwo = 0;
-        
         unordered_map<int, int> hashMap;
-        while(pointerTwo <= k){
-            hashMap[nums[pointerTwo]]++;
-            if(hashMap[nums[pointerTwo]] > 1){
-                return true;
-            }
-            pointerTwo++;
-        }
-
-        while(pointerTwo < nums.size()){
-            if(hashMap[nums[pointerOne]] > 0){
-                hashMap[nums[pointerOne]]--;
-            }
-            hashMap[nums[pointerTwo]]++;
-            
-            if(hashMap[nums[pointerTwo]] > 1){
+        for(int i = 0; i < nums.size(); i++){
+            if(hashMap.find(nums[i]) != hashMap.end() && i - hashMap[nums[i]] <= k){
                 return true;
             }
 
-            pointerOne++;
-            pointerTwo++;
+            hashMap[nums[i]] = i;
         }
 
         return false;
