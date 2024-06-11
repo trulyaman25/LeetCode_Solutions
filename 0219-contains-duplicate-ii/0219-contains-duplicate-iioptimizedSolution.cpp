4a5822931c9dc2bfaaf7b45ef23f9833d954cb2d
Aman Kumar <trulyaman25@gmail.com>
2024-06-11 17:04:28 +0530

    Time: 102 ms (95.5%), Space: 80.9 MB (60.36%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-iioptimizedSolution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> hashMap;
+        for(int i = 0; i < nums.size(); i++){
+            if(hashMap.find(nums[i]) != hashMap.end() && i - hashMap[nums[i]] <= k){
+                return true;
+            }
+
+            hashMap[nums[i]] = i;
+        }
+
+        return false;
+    }
+};
