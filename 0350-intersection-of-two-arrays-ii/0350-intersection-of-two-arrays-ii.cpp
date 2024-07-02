c7ede0f9f2c53290f57ae8d3050185382bd2c624
Aman Kumar <trulyaman25@gmail.com>
2024-07-02 15:01:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> frequencyVectorOne(1001, 0);
-        vector<int> frequencyVectorTwo(1001, 0);
-
-        for(int i = 0; i < nums1.size(); i++){
-            frequencyVectorOne[nums1[i]]++;
-        }
-
-        for(int i = 0; i < nums2.size(); i++){
-            frequencyVectorTwo[nums2[i]]++;
-        }
-
         vector<int> resultantVector;
-        for(int i = 0; i < 1001; i++){
-            int count = min(frequencyVectorOne[i], frequencyVectorTwo[i]);
-            while(count > 0){
-                resultantVector.push_back(i);
-                count--;
+        unordered_map<int, int> hashMap;
+
+        if(nums1.size() > nums2.size()){
+            for(int i = 0; i < nums1.size(); i++){
+                hashMap[nums1[i]]++;
+            }
+            for(int i = 0; i < nums2.size(); i++){
+                if(hashMap.count(nums2[i]) > 0){
+                    resultantVector.push_back(nums2[i]);
+                }
+            }
+        } else {
+            for(int i = 0; i < nums2.size(); i++){
+                hashMap[nums2[i]]++;
+            }
+            for(int i = 0; i < nums1.size(); i++){
+                if(hashMap.count(nums1[i]) > 0){
+                    resultantVector.push_back(nums1[i]);
+                }
             }
         }
+
         return resultantVector;
     }
 };
