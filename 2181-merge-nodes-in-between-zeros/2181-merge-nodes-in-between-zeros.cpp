f2ffda32027d0b18677248eada14df29fa878cba
Aman Kumar <trulyaman25@gmail.com>
2024-07-04 13:52:06 +0530

    Time: 616 ms (5.02%), Space: 291.3 MB (6.05%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
@@ -1,24 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* mergeNodes(ListNode* head) {
         ListNode* tempHead = head->next;
-        int sum = 0;
 
-        ListNode* newHead = new ListNode();
-        ListNode* mover = newHead;
+        long long sum = 0;
+        vector<long long> resultantVector;
         while(tempHead){
-            sum += tempHead->val;
-
             if(tempHead->val == 0){
-                ListNode* newNode = new ListNode(sum);
-                mover->next = newNode;
-                mover = mover->next;
+                resultantVector.push_back(sum);
                 sum = 0;
+            } else {
+                sum += tempHead->val;
             }
-
             tempHead = tempHead->next;
         }
 
-        return newHead->next;
+        for(auto it : resultantVector){
+            cout << it << " ";
+        }
+
+        ListNode* newHead = new ListNode(resultantVector[0]);
+        ListNode* mover = newHead;
+        for(int i = 1; i < resultantVector.size(); i++){
+            ListNode* newNode = new ListNode(resultantVector[i]);
+            mover->next = newNode;
+            mover = mover->next;
+        }
+
+        return newHead;
     }
 };
