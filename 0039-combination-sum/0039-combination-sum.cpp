2dbac03eec96c6bb0be6f39c27f4339115cf8dcf
Aman Kumar <trulyaman25@gmail.com>
2024-07-22 15:31:39 +0530

    Time: 0 ms (100%), Space: 12.4 MB (59.55%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-
-    void printSubSequence(int index, int targetSum, vector<int> &assetVector, vector<int> &printingVector, vector<vector<int>> &resultantVector){
+    void findCombination(int index, int targetSum, vector<int>& assetVector, vector<int>& printingVector, vector<vector<int>>& resultantVector){
         if(index == assetVector.size()){
             if(targetSum == 0){
                 resultantVector.push_back(printingVector);
@@ -11,17 +10,18 @@ public:
 
         if(assetVector[index] <= targetSum){
             printingVector.push_back(assetVector[index]);
-            printSubSequence(index, targetSum - assetVector[index], assetVector, printingVector, resultantVector);
+            findCombination(index, targetSum - assetVector[index], assetVector, printingVector, resultantVector);
             printingVector.pop_back();
         }
-        
-        printSubSequence(index + 1, targetSum, assetVector, printingVector, resultantVector);
+
+        findCombination(index + 1, targetSum, assetVector, printingVector, resultantVector);
+
     }
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<int> dataVector;
         vector<vector<int>> resultantVector;
-        printSubSequence(0, target, candidates, dataVector, resultantVector);
+        findCombination(0, target, candidates, dataVector, resultantVector);
 
         return resultantVector;
     }
