141aff54ab08a0981c69507657cbce5062ac85ff
Aman Kumar <trulyaman25@gmail.com>
2024-08-14 11:02:46 +0530

    Time: 137 ms (72.94%), Space: 70.3 MB (62.14%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -5,28 +5,26 @@ public:
     }
 
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        vector<vector<int>> resultantVector;
         priority_queue<pair<int, pair<int, int>>> maxHeap;
 
         for(int i = 0; i < points.size(); i++){
             int distance = square(points[i][0]) + square(points[i][1]);
-            maxHeap.push(make_pair(abs(distance), make_pair(points[i][0], points[i][1])));
+            maxHeap.push(make_pair(distance, make_pair(points[i][0], points[i][1])));
             
             if(maxHeap.size() > k){
                 maxHeap.pop();
             }
         }
 
-        while(maxHeap.size() > 0){
-            vector<int> dataVector;
-            dataVector.push_back(maxHeap.top().second.first);
-            dataVector.push_back(maxHeap.top().second.second);
+        points.clear();
 
-            resultantVector.push_back(dataVector);
+        while(maxHeap.size() > 0){
+            vector<int> dataVector = {maxHeap.top().second.first, maxHeap.top().second.second};
+            points.push_back(dataVector);
 
             maxHeap.pop();
         }
 
-        return resultantVector;
+        return points;
     }
 };
