d90e4d9926dea576baf5d8e7bec3cc80ed20b88d
Aman Kumar <trulyaman25@gmail.com>
2024-08-14 11:02:34 +0530

    Time: 151 ms (58.07%), Space: 73.6 MB (52.14%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -9,8 +9,8 @@ public:
         priority_queue<pair<int, pair<int, int>>> maxHeap;
 
         for(int i = 0; i < points.size(); i++){
-            int distance = sqrt(square(points[i][0]) + square(points[i][1]));
-            maxHeap.push(make_pair(distance, make_pair(points[i][0], points[i][1])));
+            int distance = square(points[i][0]) + square(points[i][1]);
+            maxHeap.push(make_pair(abs(distance), make_pair(points[i][0], points[i][1])));
             
             if(maxHeap.size() > k){
                 maxHeap.pop();
