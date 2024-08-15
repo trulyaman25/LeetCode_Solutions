2e860f96171285fcc905bc24d580a6d04f35864d
Aman Kumar <trulyaman25@gmail.com>
2024-08-15 10:54:00 +0530

    Time: 65 ms (83.8%), Space: 86.1 MB (32.71%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cpp
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -3,27 +3,29 @@ public:
     bool lemonadeChange(vector<int>& bills) {
         int fiveCount = 0;
         int tenCount = 0;
+        
         for(int i = 0; i < bills.size(); i++){
             if(bills[i] == 5){
                 fiveCount++;
-            } else if(bills[i] == 10 && fiveCount > 0){
+            } else if(bills[i] == 10 && fiveCount >= 1){
                 tenCount++;
                 fiveCount--;
             } else {
-                if(tenCount > 0){
+                if(tenCount >= 1){
                     tenCount--;
-                    if(fiveCount > 0 && fiveCount > 0){
+                    if(fiveCount >= 1){
                         fiveCount--;
                     } else {
                         return false;
                     }
-                } else if(fiveCount >= 3 && fiveCount > 0){
+                } else if(fiveCount >= 3){
                     fiveCount -= 3;
                 } else {
                     return false;
                 }
             }
         }
+
         return true;
     }
 };
