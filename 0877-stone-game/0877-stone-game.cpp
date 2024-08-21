193cd04ea7eb58d2bf2ab4b6716248d97052a980
Aman Kumar <trulyaman25@gmail.com>
2024-08-21 09:45:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,39 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
+        int aliceCount = 0;
+        int bobCount = 0;
+
+        int count = piles.size();
+        bool flag = true;
+        while(count--){
+            if(flag){
+                if(piles[0] > piles.back()){
+                    aliceCount += piles[0];
+                    piles.erase(piles.begin());
+                } else {
+                    aliceCount += piles.back();
+                    piles.pop_back();
+                }
+                
+                flag = false;
+            } else {
+                if(piles[0] > piles.back()){
+                    bobCount += piles[0];
+                    piles.erase(piles.begin());
+                } else {
+                    bobCount += piles.back();
+                    piles.pop_back();
+                }
+                
+                flag = true;
+            }
+        }
+
+        if(aliceCount < bobCount){
+            return false;
+        }
+
         return true;
     }
 };
