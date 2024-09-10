475e57ad343100145bbe2e3c955bac640ccb2727
Aman Kumar <trulyaman25@gmail.com>
2024-09-10 08:36:20 +0530

    Time: 51 ms (20.97%), Space: 35.5 MB (82.75%) - LeetHub

--- 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.cpp
+++ 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.cpp
@@ -1,16 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
+    int findGCD(int numberOne, int numberTwo){
+        while(numberTwo != 0){
+            int temp = numberTwo;
+            numberTwo = numberOne % numberTwo;
+            numberOne = temp;
+        }
+        return numberOne;
+    }
+
     ListNode* insertGreatestCommonDivisors(ListNode* head) {
-        if(head == NULL || head->next == NULL){
+        if(head->next == NULL){
             return head;
         }
 
         ListNode* tempHead = head;
         while(tempHead->next){
-            int requiredGCD = __gcd(tempHead->val, tempHead->next->val);
-            ListNode* newNode = new ListNode(requiredGCD, tempHead->next);
+            int gcdValue = findGCD(tempHead->val, tempHead->next->val);
 
+            ListNode* newNode = new ListNode(gcdValue, tempHead->next);
             tempHead->next = newNode;
+
             tempHead = newNode->next;
         }
 
