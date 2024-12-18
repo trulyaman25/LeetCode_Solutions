7559de32c2fc010bb5c01374643894b233df4cf9
Aman Kumar <trulyaman25@gmail.com>
2024-12-19 02:37:38 +0530

    Time: 0 ms (100%), Space: 26.1 MB (35.36%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,43 +1,28 @@
-//Brute Force
-// class Solution {
-// public:
-//     int singleNonDuplicate(vector<int>& nums) {
-//         if(nums.size() == 1){
-//             return nums.back();
-//         }
-
-//         for(int i = 0; i < nums.size(); i += 2){
-//             if(i == nums.size() - 1){
-//                 return nums.back();
-//             }
-
-//             if(nums[i] != nums[i + 1]){
-//                 return nums[i];
-//             }
-//         }
-//         return -1;
-//     }
-// };
-
-//Optimized Solution
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int leftPointer = 0;
         int rightPointer = nums.size() - 1;
 
+        int mid = -1;
         while (leftPointer < rightPointer) {
-            int mid = (leftPointer + rightPointer) / 2;
-            if (mid % 2 == 1) {
-                mid--;
-            }
+            mid = leftPointer + (rightPointer - leftPointer) / 2;
             
-            if (nums[mid] != nums[mid + 1]) {
-                rightPointer = mid;
+            if (mid % 2 == 0) {
+                if (mid < nums.size() - 1 && nums[mid] == nums[mid + 1]) {
+                    leftPointer = mid + 2;
+                } else {
+                    rightPointer = mid;
+                }
             } else {
-                leftPointer = mid + 2;
+                if (nums[mid] != nums[mid + 1]) {
+                    leftPointer = mid + 1;
+                } else {
+                    rightPointer = mid;
+                }
             }
         }
+
         return nums[leftPointer];
     }
 };
