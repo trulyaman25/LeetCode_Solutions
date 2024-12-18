46409b0dfa6a5d663189fbc1ccdd20b908e80952
Aman Kumar <trulyaman25@gmail.com>
2024-12-19 02:30:41 +0530

    Time: 15 ms (5%), Space: 24.5 MB (100%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,19 +1,43 @@
+//Brute Force
+// class Solution {
+// public:
+//     int singleNonDuplicate(vector<int>& nums) {
+//         if(nums.size() == 1){
+//             return nums.back();
+//         }
+
+//         for(int i = 0; i < nums.size(); i += 2){
+//             if(i == nums.size() - 1){
+//                 return nums.back();
+//             }
+
+//             if(nums[i] != nums[i + 1]){
+//                 return nums[i];
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+//Optimized Solution
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        if(nums.size() == 1){
-            return nums.back();
-        }
+        int leftPointer = 0;
+        int rightPointer = nums.size() - 1;
 
-        for(int i = 0; i < nums.size(); i += 2){
-            if(i == nums.size() - 1){
-                return nums.back();
+        while (leftPointer < rightPointer) {
+            int mid = (leftPointer + rightPointer) / 2;
+            if (mid % 2 == 1) {
+                mid--;
             }
             
-            if(nums[i] != nums[i + 1]){
-                return nums[i];
+            if (nums[mid] != nums[mid + 1]) {
+                rightPointer = mid;
+            } else {
+                leftPointer = mid + 2;
             }
         }
-        return -1;
+        return nums[leftPointer];
     }
 };
