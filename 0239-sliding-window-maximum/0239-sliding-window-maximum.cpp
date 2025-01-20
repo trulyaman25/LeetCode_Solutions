bf27aba59b1297cfad9e4d137d7e339e76a56bf8
Aman Kumar <trulyaman25@gmail.com>
2025-01-21 00:13:04 +0530

    Time: 33 ms (36.36%), Space: 139.1 MB (78.82%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -3,31 +3,32 @@ public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         int pointerOne = 0;
         int pointerTwo = 0;
-        
+
         vector<int> resultantVector;
         deque<int> deque;
         while (pointerTwo < k) {
-            while (!deque.empty() && nums[pointerTwo] >= deque.back()) {
+            while (!deque.empty() && nums[pointerTwo] >= nums[deque.back()]) {
                 deque.pop_back();
             }
-            deque.push_back(nums[pointerTwo]);
+            
+            deque.push_back(pointerTwo);
             pointerTwo++;
         }
 
-        resultantVector.push_back(deque.front());
+        resultantVector.push_back(nums[deque.front()]);
 
         while (pointerTwo < nums.size()) {
-            if (deque.front() == nums[pointerOne]) {
+            while (!deque.empty() && deque.front() <= pointerOne) {
                 deque.pop_front();
             }
 
-            while (!deque.empty() && nums[pointerTwo] >= deque.back()) {
+            while (!deque.empty() && nums[deque.back()] <= nums[pointerTwo]) {
                 deque.pop_back();
             }
 
-            deque.push_back(nums[pointerTwo]);
+            deque.push_back(pointerTwo);
 
-            resultantVector.push_back(deque.front());
+            resultantVector.push_back(nums[deque.front()]);
 
             pointerOne++;
             pointerTwo++;
