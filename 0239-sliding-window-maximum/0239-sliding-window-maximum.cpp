eeba184daac75100d33baad8ac7fe2403e63ff4e
Aman Kumar <trulyaman25@gmail.com>
2025-01-20 23:59:37 +0530

    Time: 24 ms (61.55%), Space: 139.2 MB (59.01%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int pointerOne = 0;
+        int pointerTwo = 0;
+        
+        vector<int> resultantVector;
+        deque<int> deque;
+        while (pointerTwo < k) {
+            while (!deque.empty() && nums[deque.back()] <= nums[pointerTwo]) {
+                deque.pop_back();
+            }
+            deque.push_back(pointerTwo);
+            pointerTwo++;
+        }
+
+        resultantVector.push_back(nums[deque.front()]);
+
+        while (pointerTwo < nums.size()) {
+            while (!deque.empty() && deque.front() <= pointerOne) {
+                deque.pop_front();
+            }
+
+            while (!deque.empty() && nums[deque.back()] <= nums[pointerTwo]) {
+                deque.pop_back();
+            }
+
+            deque.push_back(pointerTwo);
+
+            resultantVector.push_back(nums[deque.front()]);
+
+            pointerOne++;
+            pointerTwo++;
+        }
+
+        return resultantVector;
+    }
+};
