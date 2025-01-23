7e217933e0fdda62512b4458ca355d980df4b7cb
Aman Kumar <trulyaman25@gmail.com>
2025-01-24 01:58:11 +0530

    Time: 1 ms (38.27%), Space: 59.2 MB (58.29%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    int longestOnes(vector<int>& nums, int k) {
-        int leftPointer = 0;
-        int rightPointer = 0;
+    int longestOnes(vector<int>& nums, int life) {
+        int pointerOne = 0;
+        int pointerTwo = 0;
 
-        int zeroCount = 0;
-        int maxLength = INT_MIN;
-        for(int rightPointer = 0; rightPointer < nums.size(); rightPointer++){
-            if(nums[rightPointer] == 0){
-                zeroCount++;
+        int maxLength = 0;
+        while(pointerTwo < nums.size()){
+            if(nums[pointerTwo] == 0){
+                life--;
             }
 
-            if(zeroCount <= k){
-                int length = rightPointer - leftPointer + 1;
-                maxLength = max(maxLength, length);
-            } else {
-                while(zeroCount > k){
-                    if(nums[leftPointer++] == 0){
-                        zeroCount--;
-                    }
+            while(life < 0){
+                if(nums[pointerOne] == 0){
+                    life++;
                 }
+                pointerOne++;
             }
+
+            maxLength = max(pointerTwo - pointerOne + 1, maxLength);
+            pointerTwo++;
         }
 
         return maxLength;
