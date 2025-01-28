ba52efb6ae198a1761355c35c1421ebe74a2e983
Aman Kumar <trulyaman25@gmail.com>
2025-01-29 02:45:10 +0530

    Time: 0 ms (100%), Space: 11.7 MB (41.01%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,30 +1,3 @@
-//2 POINTER APPROACH (ARRANGES ALL THE VAL AT THE BACK OF THE VECTOR)
-// class Solution {
-// public:
-//     int removeElement(vector<int>& nums, int val) {
-//         int pointerOne = 0;
-//         int pointerTwo = nums.size() - 1;
-//         int valuableCount = 0;
-
-//         while(pointerOne <= pointerTwo){
-//             if(nums[pointerOne] == val && nums[pointerTwo] != val){
-//                 swap(nums[pointerOne], nums[pointerTwo]);
-//                 valuableCount++;
-//                 pointerOne++;
-//                 pointerTwo--;
-//             } else if (nums[pointerTwo] == val) {
-//                 pointerTwo--;
-//                 valuableCount++;
-//             } else {
-//                 pointerOne++;
-//             }
-//         }
-
-//         return nums.size() - valuableCount;
-//     }
-// };
-
-//1 POINTER APPROACH (REMOVES THE OCCURRENCES OF VAL FROM THE VECTOR)
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
