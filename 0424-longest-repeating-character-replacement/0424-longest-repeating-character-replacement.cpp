a6bba278d87a7c51ae5dcdf39dbed79ef1a6f0c2
Aman Kumar <trulyaman25@gmail.com>
2025-01-31 02:08:37 +0530

    Time: 7 ms (53.23%), Space: 11.1 MB (9.38%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int maxFrequency = 0;
-        int maxLength = 0;
-
         int pointerOne = 0;
         int pointerTwo = 0;
+
+        int maxFrequency = 0;
+        int longestSubString = 0;
+
         unordered_map<char, int> charFrequency;
-        while (pointerTwo < s.length()) {
+        while(pointerTwo < s.length()){
             charFrequency[s[pointerTwo]]++;
             maxFrequency = max(maxFrequency, charFrequency[s[pointerTwo]]);
 
-            while ((pointerTwo - pointerOne + 1) - maxFrequency > k) {
+            while(pointerTwo - pointerOne + 1 - maxFrequency> k){
                 charFrequency[s[pointerOne]]--;
                 pointerOne++;
             }
 
-            maxLength = max(maxLength, pointerTwo - pointerOne + 1);
+            longestSubString = max(longestSubString, pointerTwo - pointerOne + 1);
             pointerTwo++;
         }
 
-        return maxLength;
+        return longestSubString;
     }
 };
