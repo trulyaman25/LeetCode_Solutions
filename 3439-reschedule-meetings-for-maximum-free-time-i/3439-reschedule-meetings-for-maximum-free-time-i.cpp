be64b1c4969fc62f8a663c137932cffed380e4be
Aman Kumar <trulyaman25@gmail.com>
2025-02-01 23:01:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3439-reschedule-meetings-for-maximum-free-time-i/3439-reschedule-meetings-for-maximum-free-time-i.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int maxFreeTime(int eventTime, int k, vector<int>& startTime, vector<int>& endTime) {
+        vector<int> gaps;
+        gaps.push_back(startTime[0] - 0);
+        for(int i = 0; i < startTime.size() - 1; i++){
+            int difference = startTime[i + 1] - endTime[i];
+            gaps.push_back(difference);
+        }
+        gaps.push_back(eventTime - endTime.back());
+
+        int maxSum = 0;
+        int currentSum = 0;
+        int pointerOne = 0;
+        int pointerTwo = 0;
+        while(pointerTwo < k + 1){
+            currentSum += gaps[pointerTwo];
+            pointerTwo++;
+        }
+
+        maxSum = max(maxSum, currentSum);
+
+        while(pointerTwo < gaps.size()){
+            currentSum += gaps[pointerTwo];
+            maxSum = max(maxSum, currentSum);
+            currentSum -= gaps[pointerOne];
+            pointerOne++;
+            pointerTwo++;
+        }
+
+        return maxSum;
+    }
+};
