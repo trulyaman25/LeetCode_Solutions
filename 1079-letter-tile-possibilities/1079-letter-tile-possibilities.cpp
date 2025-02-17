62e851c49092ccb8e851f36f6b469a19df7379b5
Aman Kumar <trulyaman25@gmail.com>
2025-02-17 09:46:23 +0530

    Time: 130 ms (6.28%), Space: 19.1 MB (15.1%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
-    void findPermutations(int index, string dataString, int& count, set<string>& resultantVector){
-        count++;
+    void findPermutations(int index, string dataString, set<string>& resultantVector){
         resultantVector.insert(dataString);
         for(int i = index; i < dataString.length(); i++){
             swap(dataString[index], dataString[i]);
-            findPermutations(index + 1, dataString, count, resultantVector);
+            findPermutations(index + 1, dataString, resultantVector);
             swap(dataString[index], dataString[i]);
         }
     }
 
-    void findCombination(int index, string& assetString, string& dataString, int& count, set<string>& resultantVector){
-        count++;
+    void findCombination(int index, string& assetString, string& dataString, set<string>& resultantVector){
         resultantVector.insert(dataString);
         for(int i = index; i < assetString.length(); i++){
             if(i > index && assetString[i] == assetString[i - 1]){
@@ -20,20 +18,19 @@ public:
 
             dataString += assetString[i];
             if(dataString.length() >= 2){
-                findPermutations(0, dataString, count, resultantVector);
+                findPermutations(0, dataString, resultantVector);
             }
-            findCombination(i + 1, assetString, dataString, count, resultantVector);
+            findCombination(i + 1, assetString, dataString, resultantVector);
             dataString.pop_back();
         }
     }
 
     int numTilePossibilities(string s) {
-        int count = 0;
         sort(s.begin(), s.end());
 
         string tempString;
         set<string> resultantVector;
-        findCombination(0, s, tempString, count, resultantVector);
+        findCombination(0, s, tempString, resultantVector);
 
         return resultantVector.size() - 1;
     }
