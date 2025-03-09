9d09a6678148489adfd6e58d9548db11ccc5c39a
Aman Kumar <trulyaman25@gmail.com>
2025-03-09 23:23:09 +0530

    Time: 53 ms (97.7%), Space: 108 MB (38.7%) - LeetHub

---
+++ 3208-alternating-groups-ii/3208-alternating-groups-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int numberOfAlternatingGroups(vector<int>& nums, int k) {
+        int windowSize = k;
+        for(int i = 0; i < windowSize - 1; i++){
+            nums.push_back(nums[i]);
+        }
+
+        int groupCount = 0;
+        int pointerOne = 0;
+        int pointerTwo = 1;
+        while(pointerTwo < nums.size()){
+            if(nums[pointerTwo] == nums[pointerTwo - 1]){
+                pointerOne = pointerTwo;
+            }
+
+            if(pointerTwo - pointerOne + 1 < windowSize){
+                pointerTwo++;
+            } else {
+                groupCount++;
+                
+                pointerOne++;
+                pointerTwo++;
+            }
+        }
+
+        return groupCount;
+    }
+};
