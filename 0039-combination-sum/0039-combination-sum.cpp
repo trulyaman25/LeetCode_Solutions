2ef6b9f6b94defe7ae8260908acecf19ceb20db8
Aman Kumar <trulyaman25@gmail.com>
2025-03-10 01:43:15 +0530

    Time: 0 ms (100%), Space: 13.9 MB (91.33%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    void findCombination(int index, int targetSum, vector<int>& assetVector, vector<int>& printingVector, vector<vector<int>>& resultantVector){
-        if(index == assetVector.size()){
+    void findCombinationalSum(vector<int>& assetVector, int targetSum, vector<vector<int>>& resultantVector, vector<int>& tempVector, int index){
+        if(index >= assetVector.size()){
             if(targetSum == 0){
-                resultantVector.push_back(printingVector);
+                resultantVector.push_back(tempVector);
             }
             return;
         }
 
-        if(assetVector[index] <= targetSum){
-            printingVector.push_back(assetVector[index]);
-            findCombination(index, targetSum - assetVector[index], assetVector, printingVector, resultantVector);
-            printingVector.pop_back();
+        if(targetSum >= assetVector[index]){
+            tempVector.push_back(assetVector[index]);
+            findCombinationalSum(assetVector, targetSum - assetVector[index], resultantVector, tempVector, index);
+            tempVector.pop_back();
         }
 
-        findCombination(index + 1, targetSum, assetVector, printingVector, resultantVector);
-
+        findCombinationalSum(assetVector, targetSum, resultantVector, tempVector, index + 1);
     }
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int> dataVector;
+        vector<int> tempVector;
         vector<vector<int>> resultantVector;
-        findCombination(0, target, candidates, dataVector, resultantVector);
+        findCombinationalSum(candidates, target, resultantVector, tempVector, 0);
 
         return resultantVector;
     }
