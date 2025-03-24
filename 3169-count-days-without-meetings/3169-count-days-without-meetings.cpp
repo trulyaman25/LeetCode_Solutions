d5bbf8c3f41438593831ad44db68a86793871d4e
Aman Kumar <trulyaman25@gmail.com>
2025-03-25 01:28:59 +0530

    Time: 63 ms (79.59%), Space: 126.6 MB (59.86%) - LeetHub

--- 3169-count-days-without-meetings/3169-count-days-without-meetings.cpp
+++ 3169-count-days-without-meetings/3169-count-days-without-meetings.cpp
@@ -5,25 +5,19 @@ public:
 
         sort(meetings.begin(), meetings.end());
 
-        vector<vector<int>> resultantVector;
-     resultantVector.push_back(meetings[0]);
-
-        for (int i = 1; i < meetings.size(); ++i) {
-            if (meetings[i][0] <= resultantVector.back()[1]) {
-             resultantVector.back()[1] = max(resultantVector.back()[1], meetings[i][1]);
-            } else {
-             resultantVector.push_back(meetings[i]);
+        int freeDays = 0;
+        int lastMeeting = 0;
+        for(int i = 0; i < meetings.size(); i++){
+            if(meetings[i][0] > lastMeeting + 1){
+                freeDays += meetings[i][0] - lastMeeting - 1;
             }
+            lastMeeting = max(lastMeeting, meetings[i][1]);
         }
 
-        int count = resultantVector[0][0] - 1;
-        for (int i = 0; i < resultantVector.size() - 1; ++i) {
-            if (resultantVector[i][1] < resultantVector[i + 1][0] - 1) {
-                count += resultantVector[i + 1][0] - resultantVector[i][1] - 1;
-            }
+        if(lastMeeting < days){
+            freeDays += days - lastMeeting;
         }
-        count += days - resultantVector.back()[1];
 
-        return count;
+        return freeDays;
     }
 };
