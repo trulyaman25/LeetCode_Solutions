ea9e8fed10ff3de29e099208792365930d19b532
Aman Kumar <trulyaman25@gmail.com>
2025-05-11 23:14:31 +0530

    Time: 0 ms (100%), Space: 11.9 MB (20.63%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
@@ -4,15 +4,9 @@ public:
         if(arr.size() < 3){
             return false;
         }
-        
-        int count = 0;
-        for(int i = 0; i < arr.size(); i++){
-            if(arr[i] % 2 == 0){
-                count = 0;
-            } else {
-                count++;
-            }
-            if(count == 3){
+
+        for(int i = 0; i < arr.size() - 2; i++){
+            if(arr[i] % 2 != 0 && arr[i + 1] % 2 != 0 && arr[i + 2] % 2 != 0){
                 return true;
             }
         }
