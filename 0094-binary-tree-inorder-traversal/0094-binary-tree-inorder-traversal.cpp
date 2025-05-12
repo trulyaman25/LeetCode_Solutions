d61059c736f4869921cd763aa670e11b5dcf7d01
Aman Kumar <trulyaman25@gmail.com>
2025-05-12 18:07:35 +0530

    Time: 0 ms (100%), Space: 10.9 MB (34.29%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -1,29 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    void traverse(TreeNode* root, vector<int>& resultantVector){
-        stack<TreeNode*> stack;
-        TreeNode* tempNode = root;
-
-        while(true){
-            if(tempNode != NULL){
-                stack.push(tempNode);
-                tempNode = tempNode->left;
+    void morrisTraversal(TreeNode* pointer, vector<int>& resultantVector){
+        while(pointer != NULL){
+            if(pointer->left == NULL){
+                resultantVector.push_back(pointer->val);
+                pointer = pointer->right;
             } else {
-                if(stack.empty()){
-                    break;
+                TreeNode* tempPointer = pointer->left;
+
+                while(tempPointer->right && tempPointer->right != pointer){
+                    tempPointer = tempPointer->right;
                 }
-                
-                tempNode = stack.top();
-                resultantVector.push_back(tempNode->val);
-                stack.pop();
-                tempNode = tempNode->right;
+
+                if(tempPointer->right == NULL){
+                    tempPointer->right = pointer;
+                    pointer = pointer->left;
+                } else {
+                    tempPointer->right = NULL;
+                    resultantVector.push_back(pointer->val);
+                    pointer = pointer->right;
+                }
             }
+
         }
     }
 
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> resultantVector;
-        traverse(root, resultantVector);
+        morrisTraversal(root, resultantVector);
 
         return resultantVector;
     }
