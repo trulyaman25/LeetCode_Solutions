acbaddf1f614c30901cbab6241451a07b40f5f33
Aman Kumar <trulyaman25@gmail.com>
2025-06-17 17:40:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cpp
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -1,28 +1,40 @@
+// TC - O(n)
+// SC - O(1)
+
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
         int fiveCount = 0;
         int tenCount = 0;
-        
+        int twentyCount = 0;
+
         for(int i = 0; i < bills.size(); i++){
-            if(bills[i] == 5){
-                fiveCount++;
-            } else if(bills[i] == 10 && fiveCount >= 1){
-                tenCount++;
-                fiveCount--;
-            } else {
-                if(tenCount >= 1){
-                    tenCount--;
-                    if(fiveCount >= 1){
-                        fiveCount--;
-                    } else {
-                        return false;
-                    }
-                } else if(fiveCount >= 3){
-                    fiveCount -= 3;
+            int billGiven = bills[i];
+            int changeRequired = billGiven - 5;
+
+            if(changeRequired == 5){
+                if(fiveCount >= 1){
+                    fiveCount--;
                 } else {
                     return false;
                 }
+            } else if(changeRequired == 15){
+                if(fiveCount >= 3){
+                    fiveCount -= 3;
+                } else if (fiveCount >= 1 && tenCount >= 1){
+                    fiveCount -= 1;
+                    tenCount -= 1;
+                } else {
+                    return false;
+                }
+            }
+
+            if(billGiven == 5){
+                fiveCount++;
+            } else if (billGiven == 10){
+                tenCount++;
+            } else {
+                twentyCount++;
             }
         }
 
