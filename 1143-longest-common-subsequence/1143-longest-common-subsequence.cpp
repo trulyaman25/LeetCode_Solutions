80909de448de9d278e745bd877338f517c0498c9
Aman Kumar <trulyaman25@gmail.com>
2025-06-25 21:54:43 +0530

    Time: 41 ms (26.51%), Space: 27.5 MB (50.84%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string text1, string text2) {
-        vector<vector<int>> dp(text1.length() + 1, vector<int>(text2.length() + 1, 0));
-
-        for(int i = 0; i < text1.size(); i++){
-            for(int j = 0; j < text2.size(); j++){
-                if(text1[i] == text2[j]){
-                    dp[i + 1][j + 1] = 1 + dp[i][j]; 
-                } else {
-                    int caseOne = dp[i + 1][j];
-                    int caseTwo = dp[i][j + 1];
-
-                    dp[i + 1][j + 1] = max(caseOne, caseTwo);
-                }
-            }
+    int getLCSS(string& textOne, string& textTwo, int indexOne, int indexTwo, vector<vector<int>>& dp){
+        if(indexOne < 0 || indexTwo < 0){
+            return 0;
         }
 
-        return dp.back().back();
+        if(dp[indexOne][indexTwo] != -1){
+            return dp[indexOne][indexTwo];
+        }
+
+        if(textOne[indexOne] == textTwo[indexTwo]){
+            return getLCSS(textOne, textTwo, indexOne - 1, indexTwo - 1, dp) + 1;
+        }
+
+        int caseOne = getLCSS(textOne, textTwo, indexOne - 1, indexTwo, dp);
+        int caseTwo = getLCSS(textOne, textTwo, indexOne, indexTwo - 1, dp);
+
+        return dp[indexOne][indexTwo] = max(caseOne, caseTwo);
     }
-}; 
+
+    int longestCommonSubsequence(string text1, string text2) {
+        vector<vector<int>> dp(text1.length(), vector<int>(text2.length(), -1));
+        return getLCSS(text1, text2, text1.length() - 1, text2.length() - 1, dp);
+    }
+};
