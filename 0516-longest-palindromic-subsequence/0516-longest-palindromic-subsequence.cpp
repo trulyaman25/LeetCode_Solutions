678c3e94bb2842edbc8411cdc8fd480a9de6336f
Aman Kumar <trulyaman25@gmail.com>
2025-06-26 19:15:11 +0530

    Time: 95 ms (22.58%), Space: 75.9 MB (62.35%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
-    int getLCSS(string& stringOne, string& stringTwo, int indexOne, int indexTwo, vector<vector<int>>& dp){
-        if(indexOne < 0 || indexTwo < 0){
-            return 0;
-        }
-
-        if(dp[indexOne][indexTwo] != -1){
-            return dp[indexOne][indexTwo];
-        }
-
-        if(stringOne[indexOne] == stringTwo[indexTwo]){
-            return 1 + getLCSS(stringOne, stringTwo, indexOne - 1, indexTwo - 1, dp);
-        }
-
-        int caseOne = getLCSS(stringOne, stringTwo, indexOne - 1, indexTwo, dp);
-        int caseTwo = getLCSS(stringOne, stringTwo, indexOne, indexTwo - 1, dp);
-
-        return dp[indexOne][indexTwo] = max(caseOne, caseTwo);
-    }
-
     int longestPalindromeSubseq(string s) {
         string reversedString = s;
         reverse(reversedString.begin(), reversedString.end());
 
-        vector<vector<int>> dp(s.length(), vector<int>(s.length(), -1));
-        return getLCSS(s, reversedString, s.length() - 1, s.length() - 1, dp);
+        vector<vector<int>> dp(s.length() + 1, vector<int>(s.length() + 1, 0));
+
+        for(int i = 1; i <= s.length(); i++){
+            for(int j = 1; j <= s.length(); j++){
+                if(s[i - 1] == reversedString[j - 1]){
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    int caseOne = dp[i - 1][j];
+                    int caseTwo = dp[i][j - 1];
+
+                    dp[i][j] = max(caseOne, caseTwo);
+                }
+            }
+        }
+
+        return dp.back().back();
     }
 };
