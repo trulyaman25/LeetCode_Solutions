fb5e9e4f5fc502ccf8483bff8480fe2f9607aa80
Aman Kumar <trulyaman25@gmail.com>
2025-06-29 01:33:36 +0530

    Time: 12 ms (6.64%), Space: 22.2 MB (9.8%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        if(prices.size() == 1){
+    int getProfit(vector<int>& prices, int index, bool buy, vector<vector<int>>& dp){
+        if(index >= prices.size()){
             return 0;
         }
 
-        int profit = 0;
-        int maxProfit = 0;
-        for(int i = 1; i < prices.size(); i++){
-            profit += prices[i] - prices[i - 1];
-            if(profit > 0){
-                maxProfit += profit;
-                profit = 0;
-            }
-            if(profit < 0){
-                profit = 0;
-            }
+        if(dp[index][buy] != -1){
+            return dp[index][buy];
         }
 
-        return maxProfit;
+        if(buy){
+            int buyStock = getProfit(prices, index + 1, false, dp) - prices[index];
+            int notBuyStock = getProfit(prices, index + 1, true, dp);
+
+            return dp[index][buy] = max(buyStock, notBuyStock);
+
+        } else {
+            int sellStock = prices[index] + getProfit(prices, index + 1, true, dp);
+            int notSellStock = getProfit(prices, index + 1, false, dp);
+
+            return dp[index][buy] = max(sellStock, notSellStock);
+        }
+    }
+
+    int maxProfit(vector<int>& prices) {
+        vector<vector<int>> dp(prices.size(), vector<int>(2, -1));
+        return getProfit(prices, 0, true, dp);
     }
 };
