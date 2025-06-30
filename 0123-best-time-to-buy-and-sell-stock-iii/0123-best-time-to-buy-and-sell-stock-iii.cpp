8042d4d0db47522e7471a4f170178ac5911ccc51
Aman Kumar <trulyaman25@gmail.com>
2025-06-30 12:40:43 +0530

    Time: 15 ms (80.1%), Space: 79.2 MB (90.93%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        vector<vector<int>> previousDP(2, vector<int>(3, 0));
+        vector<vector<int>> dp(2, vector<int>(3, 0));
+        
         for(int i = prices.size() - 1; i >= 0; i--){
-            vector<vector<int>> currentDP(2, vector<int>(3, 0));
             for(int j = 0; j < 2; j++){
                 for(int k = 1; k <= 2; k++){
                     if(j){
-                        currentDP[j][k] = max(-prices[i] + previousDP[0][k], previousDP[1][k]);
+                        dp[j][k] = max(-prices[i] + dp[0][k], dp[1][k]);
                     } else {
-                        currentDP[j][k] = max(prices[i] + previousDP[1][k - 1], previousDP[0][k]);
+                        dp[j][k] = max(prices[i] + dp[1][k - 1], dp[0][k]);
                     }
                 }
             }
-            previousDP = currentDP;
         }
 
-        return previousDP[1][2];
+        return dp[1][2];
     }
 };
