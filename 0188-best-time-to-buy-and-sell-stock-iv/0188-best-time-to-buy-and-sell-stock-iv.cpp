7f415c2451a4bd3ba1b416ab2b8ea57292432abe
Aman Kumar <trulyaman25@gmail.com>
2025-06-30 12:38:53 +0530

    Time: 14 ms (34.6%), Space: 18.7 MB (16.43%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     int maxProfit(int threshold, vector<int>& prices) {
-        vector<vector<vector<int>>> dp(prices.size() + 1, vector<vector<int>>(2, vector<int>(threshold + 1, 0)));
-
+        vector<vector<int>> dp(2, vector<int>(threshold + 1, 0));
+        
         for(int i = prices.size() - 1; i >= 0; i--){
+            vector<vector<int>> currentDP(2, vector<int>(threshold + 1, 0));
             for(int j = 0; j < 2; j++){
                 for(int k = 1; k <= threshold; k++){
                     if(j){
-                        dp[i][j][k] = max(-prices[i] + dp[i + 1][0][k], dp[i + 1][1][k]);
+                        currentDP[j][k] = max(-prices[i] + dp[0][k], dp[1][k]);
                     } else {
-                        dp[i][j][k] = max(prices[i] + dp[i + 1][1][k - 1], dp[i + 1][0][k]);
+                        currentDP[j][k] = max(prices[i] + dp[1][k - 1], dp[0][k]);
                     }
                 }
             }
+
+            dp = currentDP;
         }
 
-        return dp[0][1].back();
+        return dp[1].back();
     }
 };
