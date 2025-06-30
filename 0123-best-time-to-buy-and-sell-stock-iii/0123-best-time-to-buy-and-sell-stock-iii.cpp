1644dac0a635f12566d0a7d82d313b31b0b5aec2
Aman Kumar <trulyaman25@gmail.com>
2025-06-30 11:45:13 +0530

    Time: 547 ms (5.42%), Space: 222.7 MB (32.63%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
-    int getProfit(vector<int>& prices, int index, bool buy, int cap, vector<vector<vector<int>>>& dp){
-        if(index >= prices.size() || cap <= 0){
-            return 0;
-        }
-
-        if(dp[index][buy][cap] != -1){
-            return dp[index][buy][cap];
-        }
-
-        if(buy){
-            int buyStock = -prices[index] + getProfit(prices, index + 1, false, cap, dp);
-            int notBuyStock = getProfit(prices, index + 1, true, cap, dp);
-
-            return dp[index][buy][cap] = max(buyStock, notBuyStock);
-        } else {
-            int sellStock = prices[index] + getProfit(prices, index + 1, true, cap - 1, dp);
-            int notSellStock = getProfit(prices, index + 1, false, cap, dp);
-
-            return dp[index][buy][cap] = max(sellStock, notSellStock);
-        }
-    }
-
     int maxProfit(vector<int>& prices) {
-        vector<vector<vector<int>>> dp(prices.size(), vector<vector<int>>(2, vector<int>(3, -1)));
-        return getProfit(prices, 0, true, 2, dp);
+        vector<vector<int>> previousDP(2, vector<int>(3, 0));
+        for(int i = prices.size() - 1; i >= 0; i--){
+            vector<vector<int>> currentDP(2, vector<int>(3, 0));
+            for(int j = 0; j < 2; j++){
+                for(int k = 1; k <= 2; k++){
+                    if(j){
+                        currentDP[j][k] = max(-prices[i] + previousDP[0][k], previousDP[1][k]);
+                    } else {
+                        currentDP[j][k] = max(prices[i] + previousDP[1][k - 1], previousDP[0][k]);
+                    }
+                }
+            }
+            previousDP = currentDP;
+        }
+
+        return previousDP[1][2];
     }
 };
