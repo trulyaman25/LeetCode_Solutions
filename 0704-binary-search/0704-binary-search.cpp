3b9ad08a8e9dc9f5a831d37ed002fb8e1cbf20b2
Aman Kumar <trulyaman25@gmail.com>
2025-07-17 20:43:04 +0530

    Time: 0 ms (100%), Space: 31.4 MB (45.62%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -5,11 +5,11 @@ public:
         int pointerTwo = nums.size() - 1;
 
         while(pointerOne <= pointerTwo){
-            int mid = (pointerOne + pointerTwo ) / 2;
-            int value = nums[mid];
-            if(value == target){
+            int mid = (pointerOne + pointerTwo) / 2;
+            
+            if(nums[mid] == target){
                 return mid;
-            } else if (value > target) {
+            } else if (nums[mid] > target){
                 pointerTwo = mid - 1;
             } else {
                 pointerOne = mid + 1;
