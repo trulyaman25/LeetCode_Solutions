dc13674e4f56eaf4b4b5fcfe609093fc844b117d
Aman Kumar <trulyaman25@gmail.com>
2025-07-17 23:18:15 +0530

    Time: 0 ms (100%), Space: 31.4 MB (9.6%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
+    int getIndex(vector<int>& nums, int& target, int pointerOne, int pointerTwo){
+        if(pointerOne > pointerTwo){
+            return -1;
+        }
+
+        int mid = (pointerOne + pointerTwo) / 2;
+ 
+        if(nums[mid] == target){
+            return mid;
+        } else if (nums[mid] > target){
+            return getIndex(nums, target, pointerOne, mid - 1);
+        } else if (nums[mid] < target) {
+            return getIndex(nums, target, mid + 1, pointerTwo);
+        }
+
+        return -1;
+    }
+
     int search(vector<int>& nums, int target) {
         int pointerOne = 0;
         int pointerTwo = nums.size() - 1;
 
-        while(pointerOne <= pointerTwo){
-            int mid = (pointerOne + pointerTwo) / 2;
-            
-            if(nums[mid] == target){
-                return mid;
-            } else if (nums[mid] > target){
-                pointerTwo = mid - 1;
-            } else {
-                pointerOne = mid + 1;
-            }
-        }
-
-        return -1;
+        return getIndex(nums, target, pointerOne, pointerTwo);
     }
 };
