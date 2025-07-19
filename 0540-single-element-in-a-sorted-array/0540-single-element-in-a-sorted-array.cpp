f73cd797f1c1dc817aa9a5ce4719ddf676bc88b6
Aman Kumar <trulyaman25@gmail.com>
2025-07-19 15:24:50 +0530

    Time: 0 ms (100%), Space: 26.1 MB (67.18%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -5,17 +5,17 @@ public:
         int rightPointer = nums.size() - 1;
 
         int mid = -1;
-        while (leftPointer < rightPointer) {
+        while(leftPointer < rightPointer){
             mid = leftPointer + (rightPointer - leftPointer) / 2;
-            
-            if (mid % 2 == 0) {
-                if (mid < nums.size() - 1 && nums[mid] == nums[mid + 1]) {
+
+            if(mid % 2 == 0){
+                if(mid < nums.size() - 1 && nums[mid] == nums[mid + 1]){
                     leftPointer = mid + 2;
                 } else {
                     rightPointer = mid;
                 }
             } else {
-                if (nums[mid] != nums[mid + 1]) {
+                if(nums[mid] != nums[mid + 1]){
                     leftPointer = mid + 1;
                 } else {
                     rightPointer = mid;
