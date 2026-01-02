73b0b6564995a070577b340316d8bccc8628c7c2
Aman Kumar <trulyaman25@gmail.com>
2026-01-03 02:34:08 +0530

    Time: 0 ms (100%), Space: 30.7 MB (39.52%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -3,10 +3,10 @@ public:
     int repeatedNTimes(vector<int>& nums) {
         int max = *max_element(nums.begin(), nums.end());
         vector<int> hashArray(max + 1, 0);
-        for(int i = 1; i <= nums.size(); i++){
-            hashArray[nums[i - 1]]++;
-            if(hashArray[nums[i - 1]] == nums.size() / 2){
-                return nums[i - 1];
+        for(int i = 0; i < nums.size(); i++){
+            hashArray[nums[i]]++;
+            if(hashArray[nums[i]] == nums.size() / 2){
+                return nums[i];
             }
         }
 
